a158de4fc743033c65002b024132ca373e408cf4
suaviludius <47569681+suaviludius@users.noreply.github.com>
2019-04-25 23:35:43 +0300

    Update main.cpp

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -1,49 +1,70 @@
-#include <stdio.h>
-#include <locale.h>
-
+#include <stdio.h> 
+#include <conio.h> 
+#include <locale.h> 
 int main()
 {
-	long z; // исходное число
-	unsigned long y; // беззнаковая версия исходного числа
-	unsigned long mask = 0b0; // маска
-	int schet=0; //переменная для подсчета битов
-	int n = 0; // количество единиц
+	int solo = 0;
+	int x = 0;
+	unsigned long t = 0b0;
+	int temp = 0; //переменная для сравнения
+	unsigned int res_x = 0;
 
-	setlocale(LC_ALL, "Rus");
-	printf("Введите число z: ");
-	scanf_s("%x", &z); // ввод в шестнадцатиричном формате
-
-	y = (unsigned int)z;
-	unsigned long m = y; 
-
-	while (m != 0)
-	{
-		m=m & (~(1 << n));
-		n++;
-		schet++;
-	}
-
-	//printf("%d\n", schet);
-	n = 0;
-
-
-	while (y!=0)
-	{
-		/*0b01001011 - количество единичных битов 4
-			считаем:
-		    0b01001011 & (0b01001011 - 1) = 0b01001010
-			0b01001010 & (0b01001010 - 1) = 0b01001000
-			0b01001000 & (0b01001000 - 1) = 0b01000000
-			0b01000000 & (0b01000000 - 1) = 0b00000000
-		*/
-		y= y & (y-1);
-		mask=(mask | (1 << (schet-1-n)));
-		n++;
-		//printf(" %d ", n);
-		//printf("%x\n", mask);
-	}
 	
-	z = mask;
-	printf("Результат: %x\n", z );
+	setlocale(LC_ALL, "Rus");
+	printf("Введите чиселко х: "); //ввод числа в 16 ричной форме
+	scanf_s("%x\n", &x);
+	res_x = (unsigned int)x;
+	
+
+	
+	printf("Ввод:  ");
+	for (int i = 0; i < sizeof(res_x) * 8; i++) 
+	{ 
+		
+		temp = res_x; 
+		res_x <<= 1; 
+		res_x >>= 1;
+		if (res_x == temp)
+			printf("0");
+		else
+		{
+			printf("1");
+			solo++;
+		}
+		if ((i + 1) % 8 == 0) 
+		{
+			printf(" ");
+			for (int n = i-7; n < i-7+solo; n++)
+			{
+				t = t | (1 << 31-n);
+			}
+			solo = 0;
+		}
+		
+		res_x <<= 1; 
+	}
+
+	printf("\nВывод: ");
+
+	for (int i = 0; i < sizeof(t) * 8; i++) //где сайзоф это разряды в битах одной 16ричной цифры
+	{
+		temp = t; // присваиваем переменной значение числа
+		t <<= 1;
+		t >>= 1;
+		// обнулили последний(левый) бит числа
+		if (t == temp)// если число при этом не изменилось, то значит там стоял "0", иначе "1"
+			printf("0");
+		else
+			printf("1");
+
+		if ((i + 1) % 8 == 0) // ставим разделитель после 8 бит (для удобства просмотра)
+		{
+			printf(" ");
+		}
+
+		t <<= 1; //сдвигаем на бит влево (убираем самый левый бит, в конец строчки добавляем "0")
+	}
+
+
 	return 0;
 }
